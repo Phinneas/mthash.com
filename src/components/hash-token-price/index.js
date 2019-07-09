2bacd7d8c5d88e6892cbd154e7728e4b3967cc68
DCrusher <d.amuscias@gmail.com>
2019-07-09 09:50:42 +0500

    #MTHASH-38 WIP. Reimlementation of the top section

---
+++ src/components/hash-token-price/index.js
@@ -0,0 +1,33 @@
+import React from "react";
+import Button from "../../components/button";
+
+import "./hash-token-price.scss";
+
+function HashTokenPrice() {
+  return (
+    <div className="row-preview">
+      <div className="title-wrapper">
+        <img
+          className="icon"
+          src={require("../../assets/img/home/table-icons/icon1.png")}
+          alt="sry"
+        />
+        <p className="title">
+          Hash Token &nbsp;<span>HASH</span>
+        </p>
+      </div>
+      <div className="price-wrapper">
+        <p className="price">$10.73</p>
+        <div className="difference-wrapper positive">
+          <div className="arrow" />
+          <p className="value">0.73%</p>
+        </div>
+      </div>
+      <div className="action-wrapper">
+        <Button text="Buy" type="green" />
+      </div>
+    </div>
+  );
+}
+
+export default HashTokenPrice;
